3af5a82784ce0931e961052a9ea52fd9882fdab0
Matheus-lima-moreira <6matheuslima@gmail.com>
2024-04-21 17:18:06 -0300

    :sparkles: feat: tela de login finalizada (estilo 3 comentado e estilo 1 em primeiro plano)

---
+++ lib/Pages/login.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final txtEmail = TextEditingController();
+  final txtPassword = TextEditingController();
+  bool _obscureText = true;
+
+  void _signIn(BuildContext context) async {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/background2.png"),
+            fit: BoxFit.cover,
+            // opacity: 0.5,
+          ),
+          // color: Colors.black,
+        ),
+        child: Center(
+          child: Container(
+            padding: const EdgeInsets.all(22),
+            height: 260, // 430
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // const Image(
+                //   image: AssetImage("assets/images/logo.png"),
+                // ),
+                const Text(
+                  "LOGSAN",
+                  style: TextStyle(
+                    color: Colors.black, // Colors.white
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                TextField(
+                  style: const TextStyle(
+                    color: Colors.black,
+                  ),
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    fillColor: Colors.white,
+                    filled: true,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color.fromRGBO(99, 140, 244, 1),
+                      ),
+                    ),
+                    hintText: "seuemail@email.com",
+                    hintStyle: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  controller: txtEmail,
+                ),
+                TextField(
+                  style: const TextStyle(
+                    color: Colors.black,
+                  ),
+                  decoration: InputDecoration(
+                    border: const OutlineInputBorder(),
+                    fillColor: Colors.white,
+                    filled: true,
+                    focusedBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color.fromRGBO(99, 140, 244, 1),
+                      ),
+                    ),
+                    hintText: "sua senha",
+                    hintStyle: const TextStyle(
+                      color: Colors.black,
+                    ),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _obscureText = !_obscureText;
+                        });
+                      },
+                      icon: Icon(
+                        _obscureText
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                        color: Colors.black,                        
+                      ),
+                    ),
+                    // apply padding to the right of the button
+                    suffixIconConstraints: const BoxConstraints.tightFor(
+                      width: 60,
+                      height: 48,
+                    ),
+                  ),
+                  obscureText: _obscureText,
+                  controller: txtPassword,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.all(17),
+                          backgroundColor:
+                              const Color.fromRGBO(14, 83, 255, 1), // 99, 140, 244, 1
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                        ),
+                        child: const Text(
+                          "Login",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        onPressed: () => _signIn(context),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:logsan_app/Pages/login.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      routes: {"/login": (context) => Login()},
+      initialRoute: "/login",
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        useMaterial3: false,
+      ),
+    );
   }
 }
