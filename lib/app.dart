e6ac7770c7d4974ece30f7cb0edf2cdcc756e49f
Kaued <kauedomingues98@gmail.com>
2024-04-28 14:43:15 -0300

    🔨 refactor: routes

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:logsan_app/Pages/login.dart';
+import 'package:logsan_app/Utils/app_routes.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -7,13 +8,19 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      routes: {"/login": (context) => Login()},
-      initialRoute: "/login",
+      initialRoute: AppRoutes.login,
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.light,
       theme: ThemeData(
         useMaterial3: false,
+        colorScheme: ThemeData.light().colorScheme.copyWith(
+              primary: const Color(0xff638CF4),
+              secondary: const Color(0xffFFAB00),
+            ),
       ),
+      routes: {
+        AppRoutes.login: (context) => const Login(),
+      },
     );
   }
 }
