47d5e0b4b7f5ff09c05f47b832369906c54a6034
Kaued <kauedomingues98@gmail.com>
2024-04-28 14:42:10 -0300

    ✨feat: firebase connection

---
+++ lib/Utils/firebase_config.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+
+class FireBaseConfig {
+  static const FirebaseOptions config = FirebaseOptions(
+    apiKey: "AIzaSyAxeL89M__HYaq9i9F7e4mMVtljDPvWDEM",
+    authDomain: "logsan.firebaseapp.com",
+    projectId: "logsan",
+    storageBucket: "logsan.appspot.com",
+    messagingSenderId: "277779005658",
+    appId: "1:277779005658:web:42bbcb99bf8dd58f9965e3",
+    measurementId: "G-V89QQWJY46",
+  );
+}
